49f043a384bd81dc4601326468ca91ac85e2f475
lfernandes <lfernandes.iteris@alelo.com.br>
2025-04-18 21:39:35 -0300

    refatoração build.gradle principal

--- monorepo-internal-events/src/main/java/br/com/ldf/medium/monorepo/internal_events/config/ConsumerKafkaConfig.java
+++ monorepo-internal-events/src/main/java/br/com/ldf/medium/monorepo/internal_events/config/ConsumerKafkaConfig.java
@@ -4,14 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
 import java.util.Map;
 
-@EnableKafka
 @Configuration
 @RequiredArgsConstructor
 public class ConsumerKafkaConfig {
